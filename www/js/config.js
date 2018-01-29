23b1a154b4e3f6bfdcefc1fa9ec72dd4e87e7dac
Liam Studham <liam@Liams-MacBook-Pro-7.local>
2018-01-29 13:31:10 +0000

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+var krms_config ={			
+	'ApiUrl':"http://easy-eat.uk/mobileapp/api",		
+	'DialogDefaultTitle’:"Easy Eat",	
+	'pushNotificationSenderid':"60006061860",	
+	'facebookAppId':"1669612006394979",
+	'APIHasKey':"5d3b7e65271f83ffc8f82448d48d81cd"
+};
+
